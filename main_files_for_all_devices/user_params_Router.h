0ff1535eaac6a8e6676fb2aba8b5c009090c9aaf
fizmin <129612300+fizmin@users.noreply.github.com>
2025-09-17 17:34:49 +0200

    Create user_params_Router.h

---
+++ main_files_for_all_devices/user_params_Router.h
@@ -0,0 +1,27 @@
+#ifndef USER_P_H
+#define USER_P_H
+
+//ustawienia konkretnego urządzenia. ustawić według życzenia przed wgraniem kodu na esp32
+
+#define DEBUG true
+
+#define ADDRESS 0
+#define NET_SIZE 5
+
+// 0 - ruter
+// 1 - pilot 
+// 2 - kontroler zapasowy
+// 3 - czujnik temperatury
+// 4 - odbiornik pomiarów temperatury
+
+int routing_table[NET_SIZE] = {
+    0, // do siebie: nie wysyłaj (albo zignoruj)
+    1, // do pilota: bezpośrednio
+    2, // do zapasowego: bezpośrednio
+    3, // do czujnika: bezpośrednio
+    4  // do odbiornika: bezpośrednio
+};
+
+
+
+#endif
